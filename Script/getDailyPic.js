49162be718713d5af245500c7bdcce2826ecdae5
MadalinaMP <madalinapuimp@gmail.com>
2020-09-09 13:47:21 +0300

    start

---
+++ Script/getDailyPic.js
@@ -0,0 +1,6 @@
+var baseUrl = new URL("https://api.nasa.gov/planetary/apod");
+var myAPIkey = "Fa9LKzqaJ3DaYg3eVuRv18uwPlQTgcDRId3wQIcx";
+
+baseUrl.searchParams.set("api_key", myAPIkey);
+
+function getPicOfTheDay() {}
