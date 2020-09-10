355460aea1d6a05fd7b14e5f024bb072ba76c1ce
MadalinaMP <madalinapuimp@gmail.com>
2020-09-10 14:56:19 +0300

    error handling

--- Script/getDailyPic.js
+++ Script/getDailyPic.js
@@ -14,27 +14,60 @@ function getPicOfTheDay() {
     })
     .then(function (jsonResp) {
       console.log(jsonResp);
-      if (jsonResp.error) {
-        displayError(jsonResp.error);
-      } else displayPicture(jsonResp);
+      if (jsonResp.msg) {
+        displayError(jsonResp.msg);
+      } else if (jsonResp.media_type == "image") {
+        displayPicture(jsonResp);
+      } else if (jsonResp.media_type == "video") {
+        displayVideo(jsonResp);
+      }
     })
     .catch(function () {
       displayError("Something went wrong! Try again!");
     });
 }
 
-function displayError(errorMessage) {
-  var errorDiv = document.getElementById("errorDiv");
-  errorDiv.innerText = errorMessage;
+function getInfo(current) {
+  var infoDiv = document.getElementById("infoDiv");
+
+  var theTitle = document.createElement("h1");
+  theTitle.innerText = current.title;
+
+  var theAuthor = document.createElement("h2");
+  theAuthor.innerText = current.copyright;
+
+  var theDate = document.createElement("h3");
+  theDate.innerText = current.date;
+
+  var theDetails = document.createElement("p");
+  theDetails.innerText = current.explanation;
+
+  infoDiv.appendChild(theTitle);
+  infoDiv.appendChild(theAuthor);
+  infoDiv.appendChild(theDate);
+  infoDiv.appendChild(theDetails);
 }
 
 function displayPicture(todaysPicture) {
   var picDiv = document.getElementById("picDiv");
 
+  getInfo(todaysPicture);
+
   var dailyPic = document.createElement("img");
   dailyPic.src = todaysPicture.url;
 
   picDiv.appendChild(dailyPic);
+}
+
+function displayVideo(todaysVideo) {
+  var vidDiv = document.getElementById("vidDiv");
+
+  getInfo(todaysVideo);
+
+  var dailyVid = document.createElement("iframe");
+  dailyVid.src = todaysVideo.url;
+
+  vidDiv.appendChild(dailyVid);
 }
 
 function setDateFilter() {
@@ -43,4 +76,13 @@ function setDateFilter() {
   if (pickedDate) {
     baseUrl.searchParams.set("date", pickedDate);
   }
+}
+
+function displayError(errorMessage) {
+  var errorDiv = document.getElementById("errorDiv");
+  errorDiv.innerText = errorMessage;
+}
+
+function empty(currentDiv, currentElement) {
+  currentDiv.removeChild(currentElement);
 }
